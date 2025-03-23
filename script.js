19b4485614a5bd30b17e0f4977654ebbc34d7a78
Harini <93416184+Harini1905@users.noreply.github.com>
2025-03-23 17:27:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const question = document.querySelector(".question");
 const gif = document.querySelector(".gif");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, I love you too hehe!";
+  question.innerHTML = "Yay, See you tomorrow!";
   gif.src =
     "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExdHB6amNmNDZ2aDc0cWs5bWN5bWxpZWJ4bmgxdXZ4ZjI2MHRwa290ciZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3oFzmq0tXV2tYMbWfe/giphy.gif";
 });
